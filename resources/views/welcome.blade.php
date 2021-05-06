042a73486f4cbca1102022b606894fca17319f69
Alphy Gacheru <alphygacheru@gmail.com>
2021-05-06 04:09:52 +0300

    Navbar Modification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,9 +25,9 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Homeeeeeeeeeeee</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home Changed</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in pleeeeeeease </a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in Changed </a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
